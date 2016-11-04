493f748b956407cb0f071828267e1cbe3ad77caf
unknown <ejevgbe@EE00200151.ericsson.se>
2016-11-04 21:24:43 +0200

    Initial commit

---
+++ src/engine/Brakable.java
@@ -0,0 +1,7 @@
+package engine;
+
+/**
+ * Created by ejevgbe on 4.11.2016.
+ */
+public interface Brakable {
+}
---
+++ src/engine/Tank.java
@@ -0,0 +1,7 @@
+package engine;
+
+/**
+ * Created by ejevgbe on 4.11.2016.
+ */
+public class Tank {
+}
